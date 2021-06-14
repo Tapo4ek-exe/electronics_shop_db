f6ebba4ea7479dc1a5584d11943525a1aca76648
Андрей Мантулено <tapo4ek.exe@gmail.com>
2021-06-14 14:40:10 +0300

    Добавлена проверка категории перед удалением. Добавлены проверки пустых полей для операций создания и сохранения.

--- ClientApplication/MainWindow.xaml.cs
+++ ClientApplication/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace ClientApplication
             radioButton.IsChecked = true;
         }
 
-        private void button2_Click(object sender, RoutedEventArgs e)
+        private void button2_Click(object sender, RoutedEventArgs e)    // Удаление записи
         {
             if (state == 1)
             {
@@ -45,17 +45,33 @@ namespace ClientApplication
             else
             {
                 Category category = (Category)dataGrid.SelectedItem;
+
+                // Проверка товаров удаляемой категории
+                Product[] products = ProductRepository.GetAll();
+                foreach(Product product in products)
+                    if(product.Category.ID == category.ID)
+                    {
+                        MessageBox.Show("Данная категория используется товарами!");
+                        return;
+                    }
+                
                 CategoryRepository.Delete(category.ID);
                 dataGrid.ItemsSource = CategoryRepository.GetAll();
             }
         }
 
-        private void button3_Click(object sender, RoutedEventArgs e)
+        private void button3_Click(object sender, RoutedEventArgs e)    // обновление информации о записи
         {
             try
             {
                 if (state == 1)
                 {
+                    if (ProductIsEmpty())
+                    {
+                        MessageBox.Show("Введены не все поля");
+                        return;
+                    }
+
                     int id = ((Product)dataGrid.SelectedItem).ID;
                     Product product = new Product(id, textBox.Text, textBox2.Text, textBox3.Text, new Category(CategoryRepository.GetCategoryID(textBox1_Copy.Text), textBox1_Copy.Text));
                     ProductRepository.Update(product);
@@ -63,6 +79,12 @@ namespace ClientApplication
                 }
                 else
                 {
+                    if (String.IsNullOrWhiteSpace(textBox.Text))
+                    {
+                        MessageBox.Show("Введены не все поля");
+                        return;
+                    }
+
                     int id = ((Category)dataGrid.SelectedItem).ID;
                     Category category = new Category(id, textBox.Text);
                     CategoryRepository.Update(category);
@@ -75,19 +97,19 @@ namespace ClientApplication
             }
         }
 
-        private void button4_Click(object sender, RoutedEventArgs e)
+        private void button4_Click(object sender, RoutedEventArgs e)    // товары заданной компании
         {
             Product [] products = ProductRepository.allProductsOfCompany(textBox1.Text);
             dataGrid.ItemsSource = products;
         }
 
-        private void button5_Click(object sender, RoutedEventArgs e)
+        private void button5_Click(object sender, RoutedEventArgs e)    // товары заданной категории
         {
             Product[] products = ProductRepository.allProductsInCategory(CategoryRepository.GetCategoryID(textBox4.Text));
             dataGrid.ItemsSource = products;
         }
 
-        private void radioButton_Checked(object sender, RoutedEventArgs e)
+        private void radioButton_Checked(object sender, RoutedEventArgs e)  // выбор базы товаров
         {
             state = 1;
 
@@ -116,7 +138,7 @@ namespace ClientApplication
             dataGrid.ItemsSource = products;
         }
 
-        private void radioButton1_Checked(object sender, RoutedEventArgs e)
+        private void radioButton1_Checked(object sender, RoutedEventArgs e) // выбор базы категорий
         {
             state = 2;
 
@@ -145,7 +167,7 @@ namespace ClientApplication
             dataGrid.ItemsSource = categories;
         }
 
-        private void dataGrid_MouseUp(object sender, RoutedEventArgs e)
+        private void dataGrid_MouseUp(object sender, RoutedEventArgs e) // отображение информации о текущей записи
         {
             if (state == 1)
             {
@@ -162,20 +184,41 @@ namespace ClientApplication
             }
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void button_Click(object sender, RoutedEventArgs e) // создание новой записи
         {
             if (state == 1)
             {
+                if(ProductIsEmpty())
+                {
+                    MessageBox.Show("Введены не все поля");
+                    return;
+                }
+
                 Product product = new Product(0, textBox.Text, textBox2.Text, textBox3.Text, new Category(CategoryRepository.GetCategoryID(textBox1_Copy.Text), textBox1_Copy.Text));
                 ProductRepository.Add(product);
                 dataGrid.ItemsSource = ProductRepository.GetAll();
             }
             else
             {
+                if (String.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    MessageBox.Show("Введены не все поля");
+                    return;
+                }
+
                 Category category = new Category(0, textBox.Text);
                 CategoryRepository.Add(category);
                 dataGrid.ItemsSource = CategoryRepository.GetAll();
             }
+            dataGrid.SelectedItem = dataGrid.Items[dataGrid.Items.Count - 1];
+        }
+
+        private bool ProductIsEmpty()
+        {
+            return String.IsNullOrWhiteSpace(textBox.Text) ||
+                    String.IsNullOrWhiteSpace(textBox2.Text) || 
+                    String.IsNullOrWhiteSpace(textBox1_Copy.Text) || 
+                    (CategoryRepository.GetCategoryID(textBox1_Copy.Text) == 0);
         }
     }
 }
